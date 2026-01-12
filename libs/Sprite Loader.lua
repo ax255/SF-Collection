236ff460a34b21cd8b9a5550666b975db6a6a5d7
ax255 <66443256+ax255@users.noreply.github.com>
2026-01-12 22:48:42 +0100

    format and indent

--- libs/Sprite Loader.lua
+++ libs/Sprite Loader.lua
@@ -3,7 +3,6 @@
 --@shared
 
 if CLIENT then
-
     spriteRes = 128
     spritertIndex = 1
     
@@ -19,7 +18,6 @@ if CLIENT then
     local VerticalFrames = 1024 / spriteRes
     
     function setSpriteRes(num)
-        
         if num > 1024 then
            throw("Sprite size cannot be higher than 1024") 
         end
@@ -27,11 +25,9 @@ if CLIENT then
         spriteRes = num
         HorizontalFrames = 1024 / spriteRes
         VerticalFrames = 1024 / spriteRes
-        
     end
     
     function getUV(FrameX, FrameY)
-        
         FrameX = FrameX + 1
         FrameY = FrameY + 1
 
@@ -42,74 +38,58 @@ if CLIENT then
         local VEnd = 1 / VerticalFrames * FrameY
         
         return UStart, VStart, UEnd, VEnd
-        
     end
     
     local forceSize = nil
+
     function discordEmojiUrl(EmojiID, Size)
-        
         Size = forceSize or Size or spriteRes
+
         return "https://cdn.discordapp.com/emojis/" .. EmojiID .. ".png?size=" .. Size .. "&quality=lossless" 
-        
     end
     
     function newRT(index)
-        
         render.createRenderTarget( "sprites" .. spritertIndex )
 
         hook.add("renderoffscreen", "rtInit_"..index, function()
-            
             render.selectRenderTarget("sprites" .. spritertIndex)
             render.clear(Color(0, 0, 0, 0))
             hook.remove("renderoffscreen", "rtInit_"..index)
-            
         end)
-        
     end
 
     newRT( spritertIndex )
     
     function addSprite(name, path, keepRatio)
-        
         if sprites[name] or spriteWaitlist.lookup[name] then return end
 
         spriteWaitlist.lookup[name] = path
         table.insert( spriteWaitlist.sprites, { name, path, keepRatio } )
-
     end
     
     hook.add("tick", "processSprite", function()
-        
         if timer.curtime() - spriteWaitlist.lastTime > 0.1 and not spriteWaitlist.processing then -- and player():getPos():getDistance(chip():getPos()) < 500 then
-            
             local sprite = spriteWaitlist.sprites[1]
             
             if sprite then
-    
                 processSprite( sprite[1], sprite[2], sprite[3] )
                 spriteWaitlist.processing = true
                 
                 table.remove(spriteWaitlist.sprites, 1)
                 spriteWaitlist.lookup[sprite[1]] = nil
                 spriteWaitlist.lastTime = timer.curtime()
-
             end
-            
         end
-        
     end)
     
     function processSprite(name, path, keepRatio)
-
         if nextSprite.x > HorizontalFrames-1 then
-            
             spritertIndex = spritertIndex + 1
         
             newRT( spritertIndex )
             
             nextSprite.x = 0
             nextSprite.y = 0
-
         end
         
         sprites[name] = {}
@@ -119,11 +99,9 @@ if CLIENT then
         sprites[name].rt = spritertIndex
 
         if string.find(path, "icon16") then
-            
             local mat = material.createFromImage(path, "")
 
             hook.add("renderoffscreen", "drawEmojiRT", function()
-                
                 render.selectRenderTarget( "sprites" .. spritertIndex )
                 
                 render.setFilterMag(1)
@@ -135,23 +113,18 @@ if CLIENT then
                 render.drawTexturedRect( 1024 / HorizontalFrames * sprites[name].x, 1024 / VerticalFrames * sprites[name].y, spriteRes, spriteRes )
 
                 timer.simple(0, function()
-                    
                     render.destroyTexture(mat)
-                    
                     spriteWaitlist.processing = false
                     
                     hook.remove("renderoffscreen", "drawEmojiRT")
-                    
                 end)
-                
             end)
-            
         else
             
             --local spriteMat = material.create("UnlitGeneric")
             --spriteMat:setInt("$flags", 256)
             --spriteMat:setTextureURL("$basetexture", path, function(mtl, path, w, h, layout )
-            local spriteMat = render.createMaterial(path, function(mtl, path, w, h, layout )
+            local spriteMat = render.createMaterial(path, function(mtl, path, w, h, layout)
                 local width, height = 1024, 1024
 
                 if keepRatio then
@@ -169,11 +142,8 @@ if CLIENT then
                 if layout then
                     layout(0, 0, width, height)
                 end
-                
             end, function(mat, path)
-                
                 hook.add("renderoffscreen", "drawEmojiRT", function()
-                    
                     render.selectRenderTarget( "sprites" .. spritertIndex )
                     
                     render.setFilterMag(1)
@@ -193,39 +163,27 @@ if CLIENT then
                     spriteWaitlist.processing = false
         
                     hook.remove("renderoffscreen", "drawEmojiRT")
-                    
                 end)
-                
             end)
-            
         end
         
         if nextSprite.y + 1 > VerticalFrames-1 then
-            
             nextSprite.x = nextSprite.x + 1
             nextSprite.y = 0
-            
         else
-            
             nextSprite.y = nextSprite.y + 1
-            
         end
-        
     end
     
     function drawSprite(name, x, y, w, h, showProgress)
-        
         local sprite = sprites[name]
         
         if not sprite then 
-            
             if showProgress then
-                
                 render.setFont(render.getDefaultFont())
                 render.setColor(Color(255, 255, 255, 255))
                 render.drawText(x + w / 2, y + h / 2, "loading/not found", 1)
                 render.drawText(x + w / 2, ( y + h / 2 ) + 15, "left: " .. #spriteWaitlist.sprites, 1)
-                
             end
             
             return nil
@@ -235,7 +193,5 @@ if CLIENT then
         render.drawTexturedRectUV( x, y, w, h, getUV( sprite.x, sprite.y ) )
         
         return true
-        
     end
-    
 end
