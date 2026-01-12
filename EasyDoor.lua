4f22e8221f5e0ce6d2d39c4e552b57678a1e3e0c
ax255 <66443256+ax255@users.noreply.github.com>
2026-01-12 22:41:59 +0100

    Add minium open time, format and indent fix

--- EasyDoor.lua
+++ EasyDoor.lua
@@ -7,15 +7,13 @@
 -- The door also has a wiremod input incase you want it to work with keypad or other wire mean
 
 local doorInfo = {
-    
     openMaterial = "", -- if you just put "" then when the door will open it will just make it transparent
     openingSound = "Doors.Move6", -- the sound the door will do when opening / closing
     blockAll = false, -- true will only let player trough, false will let player, props, etc.. go trough 
-    
+    minimumOpenTime = 3 -- Minium time door will hold open
 }
 
 local Whitelist = {
-    
     -- The chip automatically add your steam friends to the whitelist but you can still manually add friends here.
     -- Only the SteamID as the index matter, the name is only there to remember what steamID is to who.
     
@@ -23,7 +21,6 @@ local Whitelist = {
     ["STEAM_0:0:63254908"] = "Ax25",
     ["STEAM_0:0:11353744"] = "BigPoop",
     --[] = "",
-    
 }
 
 local Blacklist = {}
@@ -32,22 +29,17 @@ local ver = 1.5
 local Debug = false -- don't touch this
 
 if CLIENT then
-    
     local ID
     
     net.receive("sv_ID", function(len, ply) -- receiving the door ID from server and rename the chip
-        
         ID = net.readInt(len)
         setName("Easy Door:"..tostring(ID))
-        
     end)
     
     if player() ~= owner() then return end
-
     local SteamFriends = {}
     
     local function updateSteamFriend() -- Update the steam friend list
-        
         for v,i in pairs(find.allPlayers()) do
             if i:getFriendStatus() == "friend" then
                 table.merge(SteamFriends, { [i:getSteamID()] = i } )
@@ -55,13 +47,10 @@ if CLIENT then
         end
         
         timer.simple(1, function()
-            
             net.start("cl_friend")
             net.writeTable(SteamFriends)
             net.send()
-            
         end)
-        
     end
 
     updateSteamFriend()
@@ -79,7 +68,6 @@ if CLIENT then
     end)
     
     net.receive("sv_boundingBox", function() -- update the bouding box information from the server
-        
         local Table = net.readTable()
         x = Table["x"]
         y = Table["y"]
@@ -92,29 +80,24 @@ if CLIENT then
         if Table["edit"] then
             hook.add("postdrawopaquerenderables", "Debug", editBox)
         end
-
     end)
     
     function editBox() -- draw the bounding box that will be used for detection
-
         if not isValid(Door) then return end
         
         render.setColor(Color(255, 255, 255, 100))
         render.draw3DWireframeBox(Door:getPos(), Door:getAngles(), (boundingBox.min)-Vector(x, y, z), boundingBox.max+Vector(x, y, z))
         render.setColor(Color(255, 0, 0, 100))
         render.draw3DBox(Door:getPos(), Door:getAngles(), (boundingBox.min)-Vector(x, y, z), boundingBox.max+Vector(x, y, z))
-
     end
     
     -- Floating Text from 3d2d cam exemple
     
     if player() == owner() then
-        
         local scale = 0.03
         local font = render.createFont("Roboto", 256, 400, true)
     
         hook.add("PreDrawTranslucentRenderables", "", function() -- this is to show the door ID when owner has cursor on it
-            
             if not isValid(Door) then return end
             
             local tr = owner():getEyeTrace()
@@ -134,9 +117,7 @@ if CLIENT then
                 render.drawSimpleText(0, 0, "DoorID: "..ID, 1, 1)
             render.popMatrix()
         end)
-        
     end
-
 else
     
     local Friends = {}
@@ -152,6 +133,7 @@ else
         
         doorInfo.oldColor = Door:getColor()
         doorInfo.oldMaterial = Door:getMaterial()
+        doorInfo.oldRender = Door:getRenderMode()
         
         wire.adjustInputs({ "Open" }, { "NORMAL" })
         Door_wl = wire.getWirelink(chip())
@@ -168,15 +150,14 @@ else
     end
 
     net.receive("cl_friend", function() -- receiving the steam friend list from the owner client
-        
         local Table = net.readTable()
         
         for v,i in pairs(Table) do
+            if not i:isValid() then continue end
             if not Friends[v] and not Blacklist[i:getSteamID()] then
                 table.merge(Friends, { [v] = i } )
             end 
         end
-        
     end)
     
     local x,y,z = 5,5,5 -- Making by default the detection box a bit bigger to not have the need to always go in edit mode to have the door working
@@ -186,42 +167,43 @@ else
     end
     
     timer.simple(0.5, function()
-        
         local Chips = find.byClass("starfall_processor", function(ent)
             return ent:getOwner() == owner() and string.find(ent:getChipName(), "Door", 0) ~= nil and ent ~= chip()
         end)
         
         printED("Door ID:" .. " " .. ID)
+
         if not Chips[1] then -- if there is an ent in the chips table mean this is not first door placed, so not printing the commands again.
-        printED("Welcome to EasyDoor v"..ver)
-        printED("!edit ID | bounding box edit mode")
-        printED("!add name ID | to add someone into the friendlist")
-        printED("!remove name ID | to remove someone from the friendlist")
-        printED("If ID not provided command will run on the door you looking at")
-        printED("Replacing ID by all in add or remove will execute the command on all door")
+            printED("Welcome to EasyDoor v"..ver)
+            printED("!edit ID | bounding box edit mode")
+            printED("!add name ID | to add someone into the friendlist")
+            printED("!remove name ID | to remove someone from the friendlist")
+            printED("If ID not provided command will run on the door you looking at")
+            printED("Replacing ID by all in add or remove will execute the command on all door")
         end
-        
     end)
-        
+    
+    local lastOpenTime = 0
+
     hook.add("tick", "playerDetection", function() -- this hook detect if door should be opened or closed
-        
         if not Door:isValid() then return end
         
         local Detection = find.inBox( Door:localToWorld((boundingBox.min)-Vector(x, y, z)) , Door:localToWorld((boundingBox.max)+Vector(x, y, z)), function(ent)
             return ent:isPlayer() and Friends[ent:getSteamID()]
         end)
         
-        if Door_wl:inputValue("Open") == 1 then
+        if wire.readInput(chip(), "Open") == 1 then
             doorInfo.open = true
         end
 
         if doorInfo.open ~= doorInfo.openOld then
-            doorInfo.openOld = doorInfo.open
-            Door:emitSound(doorInfo.openingSound, 75, 100, 100)
             if doorInfo.open then
+                doorInfo.openOld = doorInfo.open
+                lastOpenTime = timer.curtime()
 
                 doorInfo.oldColor = Door:getColor()
                 doorInfo.oldMaterial = Door:getMaterial()
+                doorInfo.oldRender = Door:getRenderMode()
                 
                 if doorInfo.blockAll then
                 Door:setCollisionGroup(15)
@@ -231,6 +213,7 @@ else
                 chip():setCollisionGroup(20)
                 end                
                 
+                chip():setColor(Color(nil, nil, nil, 100 ))
                 Door:setColor(Color(nil, nil, nil, 100))
                 
                 if doorInfo.openMaterial ~= "" then
@@ -238,19 +221,22 @@ else
                     Door:setMaterial(doorInfo.openMaterial)
                 end
                 
-            else
-
+                Door:emitSound(doorInfo.openingSound, 75, 100, 100)
+            elseif timer.curtime() - lastOpenTime > doorInfo.minimumOpenTime then
+                doorInfo.openOld = doorInfo.open
                 Door:setCollisionGroup(0)
                 chip():setCollisionGroup(0)
                 
-                Door:setColor(Color(nil, nil, nil, 255 ))
+                chip():setColor(Color(nil, nil, nil, 255 ))
                 Door:setColor(doorInfo.oldColor)
+                Door:setRenderMode(doorInfo.oldRender)
                 
                 if doorInfo.openMaterial ~= "" then
                     Door:setMaterial(doorInfo.oldMaterial)
                     chip():setMaterial(doorInfo.oldMaterial)
                 end
-                    
+                
+                Door:emitSound(doorInfo.openingSound, 75, 100, 100) 
             end
         end
         
@@ -263,7 +249,6 @@ else
     end)
     
     function updateBoundingBox(confirm, edit) -- this function is used to update the bouding box size to the client to draw to the hud, also used to activate the edit mode client side
-            
         net.start("sv_boundingBox")
         net.writeTable( { ["x"] = x , ["y"] = y , ["z"] = z, ["confirm"] = confirm, ["edit"] = edit } )
         net.send(owner()) 
@@ -273,16 +258,13 @@ else
             printED("Type x or y or z followed by the number of unit you want to increase the detection zone")
             printED("Type confirm to save and exit")
         end
-            
     end
     
     function editMode(ply, text) -- this function is running when in edit mode, handling the commands
-        
         edit = true
         local Message = string.explode(" ", text)
 
         if ply == owner() then
-            
             if #Message > 2 then
                 for i=1,#Message do
                     if type(Message[i]) == "number" then continue end
@@ -296,8 +278,9 @@ else
                         z = tonumber(Message[i+1])
                         updateBoundingBox()
                     end
-                end    
-            return ""
+                end
+  
+                return ""
             end
            
             if Message[1] == "x" then
@@ -317,31 +300,26 @@ else
                printED("invalid command, type confirm to exit") 
             end
             
-        return ""
+            return ""
         end
-        
     end
 
     
     hook.add("PlayerSay", "command", function(ply, text) -- main commands
-        
         local Message = string.explode(" ", text)
+
         if ply == owner() and not edit then
-            
             local tr = owner():getEyeTrace()
            
             if Message[1] == "!edit" and (Message[2] == tostring(ID) or tr.Entity == Door) then
-                
                 updateBoundingBox(nil, true)
                 hook.add("PlayerSay", "confirm", editMode)
-                
-            return ""
+
+                return ""
             elseif Message[1] == "!add" and (Message[3] == tostring(ID) or Message[3] == "all" or tr.Entity == Door) then
-                
                 local Target = find.playersByName(Message[2])
                 
                 if Target[1] then
-                    
                     if not isValid(Target[1]) then return end
                     
                     if not Friends[Target[1]:getSteamID()] then
@@ -351,18 +329,15 @@ else
                     else
                         printED("Player already in friendlist.")
                     end
-                    
                 else
                     printED("Player not found.") 
                 end
-            
-            return ""
+
+                return ""
             elseif Message[1] == "!remove" and (Message[3] == tostring(ID) or Message[3] == "all" or tr.Entity == Door) then
-                
                 local Target = find.playersByName(Message[2])
                 
                 if Target[1] then
-                    
                     if not isValid(Target[1]) then return end
                     
                     if Friends[Target[1]:getSteamID()] then
@@ -372,15 +347,13 @@ else
                     else
                         printED("Player not in friendlist.")
                     end
-                    
                 else
                     printED("Player not found.") 
                 end
-            return ""
+                
+                return ""
             end
-            
         end
-        
     end)
 
     hook.add("ClientInitialized", "firstPlace", function(ply)
@@ -393,8 +366,5 @@ else
         net.start("sv_ID") -- sending the ID of the door to client
         net.writeInt(ID,64)
         net.send(ply)
-        
     end)
-    
-
 end
